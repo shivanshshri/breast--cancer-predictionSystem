7683e553af9f242be45fef345538060cb2594eff
Shivansh Shrivastav <shiva.shri0055@gmail.com>
2026-05-02 15:29:19 +0530

    aaded vercel.json

--- app.py
+++ app.py
@@ -37,6 +37,3 @@ def predict():
     message = ['Cancrouse' if pred[0] == 1 else 'Not Cancrouse']
 
     return render_template('index.html', message=message)
-
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', debug=True)
